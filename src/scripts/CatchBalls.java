cd6afbf1fe63736092df979b908b83dca03c336d
Gaelle <gaelle.clavelin@telecom-sudparis.eu>
2017-01-27 01:36:24 +0100

    catch balls v2 qui marche (au moins pour le module)

--- src/scripts/CatchBalls.java
+++ src/scripts/CatchBalls.java
@@ -91,12 +91,11 @@ public class CatchBalls extends AbstractScript {
             else if(versionToExecute == 2){
 
                 stateToConsider.robot.turn(-Math.PI/2);
-                stateToConsider.robot.moveLengthwise(-240); //bonne distance à voir
+                stateToConsider.robot.moveLengthwise(-410); //bonne distance à voir
 
                 //Attraper le module avec le côté droit
 
                 // Manoeuvre pour arriver au niveau du module et être prêt à choper le module
-                stateToConsider.robot.turn(Math.PI);
                 stateToConsider.robot.useActuator(ActuatorOrder.MID_ATTRAPE_D, true);
                 stateToConsider.robot.useActuator(ActuatorOrder.REPLI_CALLE_D, false);
                 stateToConsider.robot.useActuator(ActuatorOrder.REPOS_ATTRAPE_D, false);
@@ -106,6 +105,11 @@ public class CatchBalls extends AbstractScript {
                 stateToConsider.robot.useActuator(ActuatorOrder.PREND_MODULE_D, true);
                 stateToConsider.robot.useActuator(ActuatorOrder.REPOS_ATTRAPE_D, true);
                 stateToConsider.robot.useActuator(ActuatorOrder.LIVRE_CALLE_D, true);
+                stateToConsider.robot.useActuator(ActuatorOrder.REPOS_CALLE_D, true);
+                stateToConsider.robot.useActuator(ActuatorOrder.LIVRE_CALLE_D, true);
+
+
+
 
                 // Et remonte-le à l'aide de l'ascenceur
                 stateToConsider.robot.useActuator(ActuatorOrder.MID_ATTRAPE_G, true);
@@ -122,8 +126,8 @@ public class CatchBalls extends AbstractScript {
                 stateToConsider.robot.useActuator(ActuatorOrder.PREND_MODULE_G, false);
 
 
-                stateToConsider.robot.turn(Math.PI-0.15);
-                stateToConsider.robot.moveLengthwise(2000);
+                stateToConsider.robot.turn(Math.PI-0.12);
+                stateToConsider.robot.moveLengthwise(188);
             }
 
             // Prepare la pelleteuse avant déploiement(bras relevés mais légèrement abaissés pour ne pas bloquer la rotation de la pelle, puis pelle mise à 300°)
@@ -138,7 +142,7 @@ public class CatchBalls extends AbstractScript {
 
             // "Lèves les bras Maurice, c'est plus rigolo quand tu lèves les bras !", RIP King Julian
             stateToConsider.robot.useActuator(ActuatorOrder.TIENT_BOULES,false);
-            stateToConsider.robot.useActuator(ActuatorOrder.REPLIER_PELLETEUSE, true);
+            stateToConsider.robot.useActuator(ActuatorOrder.REPLIER_PELLETEUSE, false);
 
             if (versionToExecute == 1){
                 stateToConsider.robot.moveLengthwise(-130);
@@ -176,7 +180,7 @@ public class CatchBalls extends AbstractScript {
         }
         else if (version == 2)
         {
-            return new Circle(new Vec2(680, 1300));
+            return new Circle(new Vec2(765, 1300), 30);
             // 1640; 680
         }
         else
