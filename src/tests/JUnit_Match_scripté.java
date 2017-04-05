dc9b977fe9b5c07112cb20941ccd204818595e1e
Gaelle <gaelle.clavelin@telecom-sudparis.eu>
2017-04-05 21:47:00 +0200

    match exclusivement scripté, pour comparer avec le pathfinding

---
+++ src/tests/JUnit_Match_scripté.java
@@ -0,0 +1,78 @@
+package tests;
+
+import enums.DirectionStrategy;
+import enums.ScriptNames;
+import enums.Speed;
+import hook.Hook;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import scripts.ScriptManager;
+import strategie.GameState;
+import table.Table;
+import threads.ThreadInterface;
+import threads.dataHandlers.ThreadEvents;
+import threads.dataHandlers.ThreadSensor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gaelle on 05/04/17.
+ */
+public class JUnit_Match_scripté extends JUnit_Test{
+    private GameState mRobot;
+    private ScriptManager scriptManager;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        log.debug("JUnit_DeplacementsTest.setUp()");
+        mRobot = container.getService(GameState.class);
+
+
+
+        //La position de depart est mise dans la Table (l'updtate config va la chercher)
+        mRobot.updateConfig();
+        mRobot.robot.setPosition(Table.entryPosition);
+
+        mRobot.robot.setOrientation(-Math.PI/2);
+
+        mRobot.robot.setLocomotionSpeed(Speed.FAST_ALL);
+        scriptManager = container.getService(ScriptManager.class);
+
+        container.getService(ThreadEvents.class);
+        container.getService(ThreadSensor.class);
+        container.getService(ThreadInterface.class);
+        container.startInstanciedThreads();
+        //mRobot.robot.switchSensor();
+
+        scriptManager.getScript(ScriptNames.INITIALISE_ROBOT).goToThenExec(4, mRobot, new ArrayList<Hook>());
+    }
+
+    @Test
+    public void catchThoseBalls()
+    {
+        ArrayList<Hook> emptyList = new ArrayList<Hook>();
+        try
+        {
+            //On execute le script
+            log.debug("Ramassage des balles");
+            mRobot.robot.setDirectionStrategy(DirectionStrategy.FORCE_FORWARD_MOTION);
+            scriptManager.getScript(ScriptNames.FULLSCRIPTED).goToThenExec(0, mRobot, new ArrayList<Hook>());
+
+            returnToEntryPosition(mRobot);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    @After
+    public void finish()
+    {
+        mRobot.robot.immobilise();
+    }
+}
+
