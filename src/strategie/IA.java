6eb7a64e0ad6d335a328c9d253ff0109d036ef4e
roxasispoor <manzanoalban@gmail.com>
2017-05-17 14:06:34 +0200

    Système de zone

--- src/strategie/IA.java
+++ src/strategie/IA.java
@@ -30,7 +30,7 @@ public class IA {
         ArrayList<Vec2> cheminLivraisonBoules2=new ArrayList<Vec2>();
         ArrayList<Vec2> cheminLivraisonModuleFond=new ArrayList<Vec2>();
         ArrayList<Vec2> cheminModuleFond=new ArrayList<Vec2>();
-        ArrayList<Vec2> cheminModuleFondPresBase=new ArrayList<Vec2>();
+        //ArrayList<Vec2> cheminModulePresBase=new ArrayList<Vec2>();
         try {
              cheminCratereFond = pf.Astarfoulah(realState.robot.getPosition(),
                     Geometry.pointProche(realState.robot.getPosition(), scriptmanager.getScript(ScriptNames.SCRIPTED_GO_TO_CRATEREFOND).entryPosition(0, 0, realState.robot.getPosition())),
@@ -80,14 +80,15 @@ public class IA {
             System.out.println("BadVersionException");
 
         }
+        /*
         try {
-            cheminModuleFondPresBase = pf.Astarfoulah(realState.robot.getPosition(),
+            cheminModulePresBase = pf.Astarfoulah(realState.robot.getPosition(),
                     Geometry.pointProche(realState.robot.getPosition(), scriptmanager.getScript(ScriptNames.SCRIPTED_GO_TO_MODULEPRESBASE).entryPosition(0, 0, realState.robot.getPosition())),
                     realState.robot.getOrientation(), realState.robot.getLocomotionSpeed().translationSpeed, realState.robot.getLocomotionSpeed().rotationSpeed);
         } catch (BadVersionException v) {
             System.out.println("BadVersionException");
 
-        }
+        }Inclu dans la livraison de boules 1 */
         ArrayList<Hook> emptyHook = new ArrayList<>();
 //Là on regarde ce qui est nul ou pas et on prend en conséquence
         if(!cheminModuleFond.isEmpty() && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_MODULEFOND) && realState.table.cratereBase.isStillThere && realState.robot.getChargementModule()<=2)
@@ -102,7 +103,9 @@ public class IA {
             }
 
         }
-        else if(realState.table.cratereBaseLunaire.isStillThere && !realState.robot.isRempliDeBoules() && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_CRATEREFOND))//(!chemincratèrefond.isEmpty) On ne teste pas l'existence du chemin pathfinding car on est dans un objet?
+        else if(realState.table.cratereBaseLunaire.isStillThere && !realState.robot.isRempliDeBoules() &&
+                !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_CRATEREFOND)
+                && (!cheminCratereFond.isEmpty() ||(realState.robot.getPosition().getY()>1400||realState.robot.getPosition().getX()>1100)))//(!chemincratèrefond.isEmpty) On ne teste pas l'existence du chemin pathfinding car on est dans un objet?
         {
             try {
              //   realState.robot.followPath(cheminCratereFond, emptyHook);
@@ -114,10 +117,35 @@ public class IA {
             }
 
         }
-        else if( realState.robot.getChargementModule()>=1 && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND))//Pareil qu'au dessus !cheminLivraisonModuleFond.isEmpty() &&
+
+        else if( realState.robot.getChargementModule()>=1 && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND) )//Pareil qu'au dessus !cheminLivraisonModuleFond.isEmpty() &&
+        {
+            try {
+                 realState.robot.followPath(cheminLivraisonModuleFond, emptyHook);
+                scriptmanager.getScript(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND).goToThenExec(0,realState,emptyHook);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Unexpected exception caught dans l'ia allant vers  livraison module");
+            }
+
+        }
+        else if( realState.robot.getChargementModule()>=1 && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND) &&
+        (!cheminLivraisonModuleFond.isEmpty() ||(realState.robot.getPosition().getY()>1400||realState.robot.getPosition().getX()>1100)))//Pareil qu'au dessus !cheminLivraisonModuleFond.isEmpty() &&
         {
             try {
                // realState.robot.followPath(cheminCratereFond, emptyHook);
+                scriptmanager.getScript(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND).goToThenExec(0,realState,emptyHook);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Unexpected exception caught dans l'ia allant vers  livraison module");
+            }
+
+        }
+        else if( realState.robot.getChargementModule()>=1 && !realState.robot.dejaFait.get(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND)){
+            try {
+                 realState.robot.followPath(cheminModuleFond, emptyHook);
                 scriptmanager.getScript(ScriptNames.SCRIPTED_GO_TO_LIVRAISONMODULEFOND).goToThenExec(0,realState,emptyHook);
             }
             catch (Exception e)
